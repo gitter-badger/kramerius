6f5be474eaeb6b03f7d3819c5286aed80c7f3300
Martin Rumánek <martin@rumanek.cz>
2014-03-04 14:37:51 +0100

    Foxml-import path

--- dockerfile/initData.sh
+++ dockerfile/initData.sh
@@ -4,4 +4,8 @@ if [ ! -d "/kramerius-data/foxml-import" ]; then
   mkdir /kramerius-data/foxml-import
 fi
 
+if [ ! -d "/kramerius-data/foxml-import/530719f5-ee95-4449-8ce7-12b0f4cadb22" ]; then
+  unzip /tmp/530719f5-ee95-4449-8ce7-12b0f4cadb22.zip -d /kramerius-data/foxml-import/
+fi
+
 
