c6e44f2c52535b765d41931fc09070bc3b063397
Martin Rumánek <martin@rumanek.cz>
2014-03-14 13:07:29 +0100

    Fcrepo set to legacy-fs (due to compatibility), fedora and postgres data are now persisted

--- dockerfile/initData.sh
+++ dockerfile/initData.sh
@@ -4,8 +4,17 @@ if [ ! -d "/kramerius-data/foxml-import" ]; then
   mkdir /kramerius-data/foxml-import
 fi
 
-if [ ! -d "/kramerius-data/foxml-import/530719f5-ee95-4449-8ce7-12b0f4cadb22" ]; then
-  unzip /tmp/530719f5-ee95-4449-8ce7-12b0f4cadb22.zip -d /kramerius-data/foxml-import/
+if [ ! -d "/kramerius-data/postgres-data" ]; then
+  mkdir /kramerius-data/postgres-data
+  mv /var/lib/postgresql/9.1/main /kramerius-data/postgres-data/main
 fi
+
+if [ ! -d "/kramerius-data/fedora-data" ]; then
+  mv /home/kramerius/fedora/dataDefault /kramerius-data/fedora-data
+fi
+
+#if [ ! -d "/kramerius-data/foxml-import/530719f5-ee95-4449-8ce7-12b0f4cadb22" ]; then
+#  unzip /tmp/530719f5-ee95-4449-8ce7-12b0f4cadb22.zip -d /kramerius-data/foxml-import/
+#fi
 
 
