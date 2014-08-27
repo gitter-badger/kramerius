27340079751be80f6b363a4f35f00aee92a01070
Martin Rumánek <martin@rumanek.cz>
2014-08-27 09:26:48 +0200

    Docker - tomcat utf-8

--- dockerfile/dev/run.sh
+++ dockerfile/dev/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -v /kramerius-data:/kramerius-data  -t -i --name="kramerius-dev" -d -p 80:80 -p 8080:8080 -p 8000:8000 -u root kramerius-dev /init.sh
+docker run -v /kramerius-data:/kramerius-data  -t -i --name="kramerius-dev" -d -p 80:80 -p 8080:8080 -p 8000:8000 -u root moravianlibrary/kramerius-dev /init.sh
 
