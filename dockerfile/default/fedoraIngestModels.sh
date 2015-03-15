b96e346d4d763d63c90c9001a7a00a09564fd69c
Martin Rumanek <martin@rumanek.cz>
2015-03-15 19:36:34 +0100

    Docker - update tomcat

--- dockerfile/default/fedoraIngestModels.sh
+++ dockerfile/default/fedoraIngestModels.sh
@@ -4,7 +4,7 @@ function ingest2fedora {
   curl -vv -u "fedoraAdmin:fedoraAdmin" -H "Content-type:text/xml" -d "format=info:fedora/fedora-system:FOXML-1.1" -X POST --upload-file $1 http://localhost:8080/fedora/objects/new
 }
 
-MODELS=/tmp/installation-4.8/fedora/*.xml
+MODELS=/tmp/Installation-5.1/fedora/*.xml
 for MODEL in $MODELS
 do
   ingest2fedora $MODEL 
